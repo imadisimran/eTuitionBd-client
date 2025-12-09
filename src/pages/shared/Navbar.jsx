01c5cb6416087194bf8cc1f2bee9c6c480bcf109
imadisimran <imadisimran@gmail.com>
2025-12-09 10:05:29 +0600

    navbar modification

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { Link, NavLink } from "react-router";
 import logo from "../../assets/logo.png";
+import useAuth from "../../hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { errorAlert, successAlert } from "../../utilities/alerts";
 const Navbar = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user, logOut } = useAuth();
+  const { data: dbUser } = useQuery({
+    queryKey: [user?.email],
+    queryFn: async () => {
+      const result = await axiosSecure.get(`/user?email=${user?.email}`);
+      return result.data;
+    },
+    enabled: !!user?.email,
+  });
   const links = (
     <>
       <li>
@@ -17,6 +31,18 @@ const Navbar = () => {
       </li>
     </>
   );
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {
+        successAlert("Logout successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+        errorAlert();
+      });
+  };
+
   return (
     <header className="sticky top-0 z-50">
       <div className="navbar glass mt-5 rounded-3xl shadow-xl px-5 items-center">
@@ -54,8 +80,87 @@ const Navbar = () => {
           <ul className="menu menu-horizontal px-1">{links}</ul>
         </div>
         <div className="navbar-end gap-5">
-          <Link className="btn btn-primary" to='/login'>Login</Link>
-          <Link className="btn btn-secondary">Post Tuition</Link>
+          {user ? (
+            <div className="flex-none">
+              <div className="dropdown dropdown-end">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle"
+                >
+                  <div className="indicator">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      {" "}
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />{" "}
+                    </svg>
+                    <span className="badge badge-sm indicator-item">8</span>
+                  </div>
+                </div>
+                <div
+                  tabIndex={0}
+                  className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
+                >
+                  <div className="card-body">
+                    <span className="text-lg font-bold">8 Items</span>
+                    <span className="text-info">Subtotal: $999</span>
+                    <div className="card-actions">
+                      <button className="btn btn-primary btn-block">
+                        View cart
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="dropdown dropdown-end">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                >
+                  <div className="w-10 rounded-full">
+                    <img
+                      alt="Tailwind CSS Navbar component"
+                      src={dbUser?.icon || dbUser?.photoURL}
+                    />
+                  </div>
+                </div>
+                <ul
+                  tabIndex="-1"
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                >
+                  <li>
+                    <Link to="/dashboard/profile" className="justify-between">
+                      Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link>Dashboard</Link>
+                  </li>
+                  <li>
+                    <button onClick={handleLogOut}>Log Out</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          ) : (
+            <>
+              <Link className="btn btn-primary" to="/login">
+                Login
+              </Link>
+              <Link className="btn btn-secondary">Post Tuition</Link>
+            </>
+          )}
         </div>
       </div>
     </header>
